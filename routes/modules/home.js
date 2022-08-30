cc7f459dc612c6005013cae5530b2880bc5f4e20
darrenjon <darrenlin23@gmail.com>
2022-08-30 22:36:02 +0800

    feat: add category dropdown

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,14 +3,19 @@ const router = express.Router()
 const Category = require('../../models/category')
 const Record = require('../../models/record')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const categories = await Category.find({})
+    .lean()
+    .sort({ _id: 'asc' })
+    .then()
+    .catch(error => console.error(error))
   Record.find()
     .lean()
     .then(records => {
       for (let record of records) {
         record.date = record.date.toISOString().split('T')[0]
       }
-      res.render('index', { records })
+      res.render('index', { categories, records })
     })
     .catch(error => console.error(error))
 })
