d6fc4914bdb46885a8b706dbbd17f9dfa644becc
darrenjon <darrenlin23@gmail.com>
2022-08-31 13:52:06 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -17,7 +17,27 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  res.send('register page')
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email }).then(user => {
+    if (user) {
+      console.log('User already exists.')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    }
+  })
+    .catch(err => console.log(err))
 })
 
 //logout
